ce9b45d2b094884f51ac81bb7fd82b44881015df
Michael Girard <mgirard989@outlook.com>
2020-12-31 10:16:33 -0500

    Test Server

---
+++ Test/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using SimpleTcp;
+
+namespace Test {
+    class Program {
+        public static SimpleTcpServer server;
+        static void Main(string[] args) {
+            Init();
+
+
+            while (Console.ReadLine() != "QUIT") {
+                Console.WriteLine("Say 'QUIT'");
+            }
+        }
+
+        private static void Init() {
+            server = new SimpleTcpServer("127.0.0.1:65340");
+
+            server.Events.ClientConnected += ClientConnected;
+            server.Events.ClientDisconnected += ClientDisconnected;
+            server.Events.DataReceived += DataReceived;
+            server.Logger = Log;
+
+            server.Start();
+        }
+
+        private static void Log(string obj) {
+            Console.WriteLine("Log: " + obj);
+        }
+
+        private static void ClientConnected(object sender, ClientConnectedEventArgs e) {
+            Console.WriteLine("CLient Connected");
+        }
+
+        private static void ClientDisconnected(object sender, ClientDisconnectedEventArgs e) {
+            Console.WriteLine("Client disconnected");
+        }
+
+        private static void DataReceived(object sender, DataReceivedEventArgs e) {
+            Console.WriteLine("Client said: " + Encoding.UTF8.GetString(e.Data));
+        }
+    }
+}
